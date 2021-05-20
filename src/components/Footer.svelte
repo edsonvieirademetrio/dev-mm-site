8f60fefbe4c790508df12e3f4851c122a750dacb
edsonvieirademetrio <edsonvieirademetrio@gmail.com>
2021-05-20 09:33:40 -0300

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,148 @@
+<div class="bg-color-blue-dark pb-4 pt-5">
+    <section class="footer-section container">
+        <div class="row">
+            <div class="col-12 col-title-footer mb-20 text-center text-lg-left">                
+                <picture>   
+                <source type="image/webp" srcset="<?php echo get_template_directory_uri(); ?>/assets/v3/img/logo-matchmoney-footer-420.webp">   
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/logo-matchmoney-footer-420.png" alt="Matchmoney Seus Investimentos rendendo acima de 340% do CDI" class="logo-footer-w">
+            </picture>
+            </div>
+        </div>
+        <div class="row text-center text-xl-left">
+            <div class="col-xl-3">               
+                <div class="mb-20">
+					<a href="/contato">
+						Central de Ajuda
+					</a>
+                    
+                </div>
+                <a href="https://api.whatsapp.com/send?phone=+5511934571180&amp;text=Gostaria de informações sobre os investimentos da MatchMoney." target="_blank">
+                    <span><i class="fa fa-whatsapp h3 text-white" aria-hidden="true"></i></span>
+                    <span class="phone-footer align-super"><u>(11) 9 3457 1180</u></span>                            
+                </a>
+                <div class="row py-2">
+                    <div class="mb-10 col-12 col-lg-5 text-center text-lg-left">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/lg-bb.png" alt="Banco do Brasil">
+                    </div>
+                    <div class="col-12"></div>
+                    <div class="col-12 col-lg-5 text-center text-lg-left">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/lg-parceiro1.png" alt="Parceria com a Google Cloud">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 mb-4 mb-lg-0">
+                <h4 class="col-title-footer font-default-bold">
+                    Mapa do Site
+                </h4>
+                <div class="menu-sitemap">
+                    <ul class="menu-footer">
+						<li><a href="/investimentos">Investimentos</a></li>
+						<li><a href="/empresas">Empresas</a></li>
+						<li><a href="/blog">Blog</a></li>
+						<li><a href="/contato">Fale Conosco</a></li>
+                        <li><a href="/saiba-mais">Dúvidas Frequentes</a></li>
+                    </ul>                                   
+                </div>
+            </div>
+            <div class="col-xl-3 mb-4 mb-lg-0">
+                <h4 class="col-title-footer font-default-bold">
+                    Recursos
+                </h4>
+                <div class="menu-site">
+                    <ul class="menu-footer">
+                        <!-- <li><a href="/saiba-mais">Dúvidas frequentes</a></li>
+                        <li><a href="/blog">Blog</a></li>
+                        <li><a href="/contato">Contato</a></li> -->
+                        <li><a href="/minha-conta">Entrar na Conta</a></li>
+                        <li><a href="/minha-conta/?register=true">Criar Conta</a></li>
+                        <li><a href="/politica-de-privacidade">Política de Privacidade</a></li>
+                        <li><a href="/termos-e-condicoes">Termos de Uso</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-3 mb-4 mb-lg-0">
+                <h4 class="col-title-footer font-default-bold">
+                    Minha Conta
+                </h4>
+                <div class="menu-account">
+                    <ul class="menu-footer mb-2">
+						<li>
+							<a href="/minha-conta/edit-address/">Dados Pessoais</a>
+						</li> 
+                        <li>
+							<a href="/minha-conta/orders/">Carteira de Investimentos</a>
+						</li>                        						
+						<li>
+							<a href="/minha-conta/edit-account/">Altere sua Senha</a>
+						</li>
+						<!--<li>Histórico de Investimentos</li>-->
+                    </ul>
+                    <div>
+                        <span class="mr-4">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/icon-security.png" alt="">
+                        </span>
+                        <div class="col-12 my-2"></div>
+                        <span>
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/icon-security-google.png" alt="">
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-auto">
+            <div class="col-xl-12">
+                <hr>
+            </div>
+            <div class="col-xl-1 col-6 text-left">
+                <span>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/v3/img/icon-brazil.png" alt="Brasil">
+                </span>                
+            </div>
+            <div class="col-xl-6">
+                <a href="">MatchMoney Intermediação de Contratos LTDA.</a>
+            </div>
+            <div class="col-12 col-xl-5 text-center text-lg-right">
+                <span class="px-2">
+                    <a href="https://www.facebook.com/Matchmoneybr/" target="blank">
+                        <i class="fa font-24 fa-facebook" aria-hidden="true"></i>
+                    </a>
+                </span>
+                <span class="px-2">
+                    <a href="https://www.instagram.com/matchmoneybr/" target="blank" class="h5">                        
+                        <i class="fa font-24 fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </span>
+                <span class="px-2">
+                    <a href="https://www.youtube.com/channel/UCy8mnpsEGzc_tqrMI0fsfDw/" target="blank">
+                        <i class="fa font-24 fa-youtube-play" aria-hidden="true"></i>
+                    </a>
+                </span>                               
+                <span class="px-2">
+                    <a href="https://www.linkedin.com/company/matchmoney" target="blank">
+                        <i class="fa font-24 fa-linkedin" aria-hidden="true"></i>
+                    </a>
+                </span>
+            </div>
+            <div class="col-xl-12">
+                <hr>
+            </div>            
+            <div class="col-xl-12">
+                <div class="text-footer-bbc small">
+                Matchmoney.com.br pertence à MATCHMONEY INTERMEDIAÇÕES DE CONTRATOS LTDA, pessoa jurídica inscrita no CNPJ sob nº 32.297.300/0001-70, com endereço à Rua José Milton Pizzi, nº 126-Sala 14, Bairro Centro, Itatiba/SP, CEP 13251-211 (Matchmoney).
+                A Matchmoney não é uma instituição financeira: somos um correspondente bancário da FIDUCIA SOCIEDADE DE CRÉDITO AO MICROEMPREENDEDOR E A EMPRESA DE PEQUENO PORTE LTDA, pessoa jurídica inscrita no CNPJ sob o nº 04.307.598/0001-17, com endereço à Rua Timbiras, nº 3109, sala 203, Bairro Barro Preto, Belo Horizonte/MG, CEP. 30.140-062 nos termos da Resolução no 3.954, de 24 de fevereiro de 2011.
+                A atividade de correspondente bancário é regulada pelo Banco Central do Brasil. Não efetuamos diretamente operações de financiamento. Ao utilizar nossa plataforma, os empréstimos são realizados por meio de uma instituição financeira, devidamente autorizada pelo Banco Central do Brasil a realizar esse tipo de atividade. A avaliação é feita conforme a política de crédito da Instituição Financeira parceira. Durante o processo de contratação de qualquer serviço no aplicativo você receberá todas as condições e informações relevantes ao empréstimo.
+                <p>Se quiser acessar nossos Termos e Condições <a href="/termos-e-condicoes/" class="font-weight-bold"><u>clique aqui</u></a>.</p>
+                </div>
+            </div>
+            <!-- <div class="col-xl-2 col-12 text-center text-xl-right">
+                <a href="">
+                    Política de Privacidade
+                </a> 
+                <br>
+                <a href="">
+                    Termos de Uso					
+                </a>
+            </div> -->
+        </div>
+    </section>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import Footer from '../components/Footer.svelte';
 
 	export let segment;
 </script>
@@ -18,3 +19,5 @@
 <main>
 	<slot></slot>
 </main>
+
+<Footer />
